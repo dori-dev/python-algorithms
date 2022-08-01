494de5791308783d958f8c7b91411a0c93db3398
dori-dev <mr.dori.dev@gmail.com>
2022-08-01 19:48:03 +0430

    linear time big o

---
+++ complexity/03-linear_time.py
@@ -0,0 +1,16 @@
+"""
+Linear Time -> O(n)
+"""
+
+numbers = [2, 16, 7, 9, 8, 23, 12]
+
+
+def get_max_number(numbers: int) -> int:
+    max_number = numbers[0]
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+    return max_number
+
+
+print(get_max_number(numbers))
