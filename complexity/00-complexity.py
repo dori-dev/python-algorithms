611445052f885c72692f31c5bed45a5251cb6a45
dori-dev <mr.dori.dev@gmail.com>
2022-08-01 19:47:31 +0430

    complexity concept

---
+++ complexity/00-complexity.py
@@ -0,0 +1,23 @@
+"""
+Complexity
+    Time | Space
+Big O(operation) notation
+"""
+
+from typing import Any
+
+# The length of the numbers is important
+# for running time and space
+numbers = [2, 4, 5, 7, 11, 40, 119]
+
+
+def get_index(list_: list, element: Any) -> Any:
+    index = 0
+    for item in list_:
+        if item == element:
+            return index
+        index += 1
+    return None
+
+
+print(get_index(numbers, 40))
