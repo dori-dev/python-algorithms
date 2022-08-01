383e038f81ac323e40b284d195e12531531b0de1
dori-dev <mr.dori.dev@gmail.com>
2022-08-01 19:48:17 +0430

    polynomial time big o

---
+++ complexity/04-polynomial_time.py
@@ -0,0 +1,22 @@
+"""
+Polynomial Time -> O(n^x)
+"""
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+
+def bubble_sort(collection):
+    length = len(collection)
+    for i in range(length-1):
+        swapped = False
+        for j in range(length-i-1):
+            if collection[j] > collection[j+1]:
+                swapped = True
+                collection[j], collection[j+1] = collection[j+1], collection[j]
+        if not swapped:
+            break
+    return collection
+
+
+# O(n^2)
+print(bubble_sort(numbers))
