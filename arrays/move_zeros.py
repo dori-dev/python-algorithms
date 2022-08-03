b538596330c307f8f6274eb07c7bc303001f45bc
dori-dev <mr.dori.dev@gmail.com>
2022-08-03 14:07:13 +0430

    add move zeros algorithm

---
+++ arrays/move_zeros.py
@@ -0,0 +1,20 @@
+"""
+move_zeros([True, 3, 5, 0, 3, False, None, 0, 'dori', 0]) =>
+    [True, 3, 5, 3, False, None, 'dori', 0, 0, 0]
+"""
+
+
+def move_zeros(sequence: list) -> list:
+    result = []
+    zeros_count = 0
+    for item in sequence:
+        if item == 0 and not isinstance(item, bool):
+            zeros_count += 1
+        else:
+            result.append(item)
+    result.extend([0] * zeros_count)
+    return result
+
+
+if __name__ == "__main__":
+    print(move_zeros([True, 3, 5, 0, 3, False, None, 0, 'dori', 0]))
